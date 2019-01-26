e1f270e32df5254837902f2bc2bfb53272ccbb10
Stephanie Gu <stephaniegu@Stephanies-MacBook-Pro-5.local>
2019-01-26 18:45:12 -0500

    Implemented api training into the JSX file

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { Jumbotron, Button, Navbar } from 'reactstrap';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Clarifai from 'clarifai';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -81,8 +82,48 @@ class WebcamCapture extends React.Component {
   }
 
   capture = () => {
-    var imageSrc = this.webcam.getScreenshot();
+    const imageSrc = this.webcam.getScreenshot();
     imagePath = imageSrc;
+    //api training
+    console.log("hi");
+
+    //api training
+    const app = new Clarifai.App({
+        apiKey: '149600ad4a084e3e8501b5255a8c4778'
+    });
+
+    // Tags that indicate a recyclable object.
+    var recycleTags = ['recycling', 'plastic', 'paper', 'cardboard', 'carton'];
+
+    var data = imageSrc.split("data:image/jpeg;base64,");
+    // Prediction model.
+    console.log(imageSrc);
+    app.models.predict(Clarifai.GENERAL_MODEL, {base64:data[1]})
+        .then(response => {
+            var concepts = response['outputs'][0]['data']['concepts'];
+            var tags = [];
+            var banned = [];
+
+            for (var i = 0; i < concepts.length; i++) {
+                for (var u = 0; u < recycleTags.length; u++) {
+                    if (concepts[i].name === recycleTags[u]) {
+                        tags.push(concepts[i]);
+                    };
+                };
+            };
+
+            // console.log(concepts);
+
+            if (tags.length !== 0) {
+                console.log("This is recyclable lulll");
+            } else {
+                console.log("Uh lol not sure lol kek");
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
     this.setState({
       imageData:imageSrc,
       imgTaken:'true',
@@ -105,37 +146,46 @@ class WebcamCapture extends React.Component {
     }
 
     var containerStyle = {
-      marginTop:100
+      marginTop:100,
+      textAlign:'center'
     }
-
     var webcamStyle = {
-
+      marginLeft:'auto',
+      marginRight:'auto',
+      width:'100%',
+      background:'blue'
+    }
+    var buttonStyle = {
+      marginTop:'3vw'
+    }
+    var retakeStyle = {
+      marginLeft:'7vw'
     }
     if (this.state.imgTaken === 'true') {
       return(
         <div style={containerStyle}>
           <img src={this.state.imageData} alt=""/>
-          <Button outline color='primary' onClick={this.retake}>{this.state.buttonName}</Button>
+          <Button style={{...retakeStyle,...buttonStyle}} outline color='primary' onClick={this.retake}>{this.state.buttonName}</Button>
         </div>
       );
     } else {
       return(
         <div style={containerStyle}>
-          <Webcam
-          ref={this.setRef}
-          audio={false}
-          height={350}
-          screenshotFormat="image/jpeg"
-          />
-          <Button outline color='primary' onClick={this.capture}>{this.state.buttonName}</Button>
+          <div>
+            <Webcam
+            style={buttonStyle}
+            ref={this.setRef}
+            audio={false}
+            height={350}
+            screenshotFormat="image/jpeg"
+            />
+          </div>
+          <Button style={buttonStyle} outline color='primary' onClick={this.capture}>{this.state.buttonName}</Button>
         </div>
       );
     }
   }
 }
-
-//exporting image file
-export default {imagePath};
 
 class TempDiv extends React.Component {
   render() {
@@ -162,6 +212,7 @@ function homePage() {
   );
 }
 
+console.log(imagePath);
 ReactDOM.render(
   <BrowserRouter>
     <div>
@@ -173,5 +224,6 @@ ReactDOM.render(
     </div>
   </BrowserRouter>,
   document.getElementById('root'));
+
 
 serviceWorker.unregister();
