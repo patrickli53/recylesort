785260393fc4709fe88976f1b8e6cd698a26d65d
Grace Kang <g8kang@uwaterloo.ca>
2019-01-26 17:07:07 -0500

    Updated apitraining

--- apitraining.js
+++ apitraining.js
@@ -4,52 +4,54 @@ const app = new Clarifai.App({
     apiKey: '149600ad4a084e3e8501b5255a8c4778'
 }); 
 
-app.inputs.create([{
-    "url": "https://i5.walmartimages.com/asr/3bc43683-1ba5-4c62-ba6a-460504e7b152_1.484d5d58b37bf0575a5364df5d69ffe2.jpeg",
-    "concepts": [
+/*app.inputs.create({
+    url: "https://spectator.imgix.net/content/uploads/2018/01/Plastic-bag-NO-TEXT-THIS_SE-copy.jpg?auto=compress,enhance,format&crop=faces,entropy,edges&fit=crop&w=820&h=550",
+    concepts: [
         {
-            "id": "trash",
-            "value": true
+            id: "trash",
+            value: true
         },
         {
-            "id": "recyclable",
-            "value": false
+            id: "recyclable",
+            value: false
         }
     ]
-}, {
-    "url": "http://www.calgary.ca/UEP/WRS/PublishingImages/What-goes-where/items/Chip-bag-160px.jpg",
-    "concepts": [
+}).then(app.models.create(
+    "garbageSortingModel",
+    [
         {
-            "id": "trash",
-            "value": true
+            "id": "trash"
         },
         {
-            "id": "recyclable",
-            "value": false
+            "id": "recyclable"
         }
     ]
-}, {
-        "url": "https://assets3.thrillist.com/v1/image/1288649/size/tmg-article_default_mobile.jpg",
-        "concepts": [
-            {
-                "id": "trash",
-                "value" : true
-            },
-            {
-                "id": "recyclable",
-                "value" : false
-            }
-        ]
-}
-]).then(
-    createModel,
-    errorHandler
-); 
+).then(
+    function (response) {
+        // do something with response
+        console.log(response);
+    },
+    function (err) {
+        // there was an error
+        console.log(err);
+    }
+));
 
+app.models.train("{model_id}").then(
+    function (response) {
+        // do something with response
+    },
+    function (err) {
+        // there was an error
+    }
+);*/
 
+//----------------------------------------------------------------------------------------------------------------
 
+// Tags that indicate a recyclable object. 
 var recycleTags = ['recycling', 'plastic', 'paper', 'cardboard', 'carton']; 
 
+// Prediction model. 
 app.models.predict(Clarifai.GENERAL_MODEL, 'https://i5.walmartimages.com/asr/3bc43683-1ba5-4c62-ba6a-460504e7b152_1.484d5d58b37bf0575a5364df5d69ffe2.jpeg')
     .then(response => {
         var concepts = response['outputs'][0]['data']['concepts']; 
@@ -64,7 +66,7 @@ app.models.predict(Clarifai.GENERAL_MODEL, 'https://i5.walmartimages.com/asr/3bc
             };  
         };  
 
-        console.log(concepts);
+        // console.log(concepts);
 
         if (tags.length != 0) {
             console.log("This is recyclable lulll");
